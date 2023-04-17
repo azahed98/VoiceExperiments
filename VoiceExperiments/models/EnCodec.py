166452c6c59600a993b4d5c8a53728157b4665a1
Arsh Zahed <arshzahed@gmail.com>
2023-04-17 03:31:54 -0700

    Finish SoundStream, logging; Begin EnCodec

---
+++ VoiceExperiments/models/EnCodec.py
@@ -0,0 +1,31 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""EnCodec model implementation.
+Adapted from https://github.com/facebookresearch/encodec/ for research purposes
+"""
+
+
+import typing as tp
+
+import numpy as np
+import torch
+from torch import nn
+
+from encodec import EncodecModel
+
+class EnCodec(EncodecMdoel):
+    def __init__(self, model_config, optimizer_configs):
+
+
+        super(EnCodec, self).__init__()
+
+    
+    def train_step(self, batch):
+        pass
+
+    def eval_step(self, batch):
+        pass
